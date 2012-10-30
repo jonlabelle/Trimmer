1c83b61f67175968206bfbb6c47ae0205814c2e9
Jon LaBelle <jon@tech0.com>
2012-10-30 10:24:56 -0500

    Added a slight timeout to save operation.

--- Trimmer.py
+++ Trimmer.py
@@ -4,20 +4,24 @@ import sublime_plugin
 
 class TrimmerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        trailing_white_space = self.view.find_all("[\t ]+$")
+        view = self.view
+
+        trailing_white_space = view.find_all("[\t ]+$")
         trailing_white_space.reverse()
 
-        edit = self.view.begin_edit()
+        edit = view.begin_edit()
 
         for r in trailing_white_space:
-            self.view.erase(edit, r)
+            view.erase(edit, r)
 
-        self.view.end_edit(edit)
+        view.end_edit(edit)
 
-        # save
+        sublime.set_timeout(lambda: self.save(), 100)
+
+    def save(self):
         if self.view.file_name() is None:
             self.view.run_command('prompt_save_as')
         else:
             self.view.run_command('save')
-
+            
         sublime.status_message('Trimmer: Remove trailing whitespace and save.')
