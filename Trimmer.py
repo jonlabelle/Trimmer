4545d11c2462ccb6d7848d185f5fe358a51af5f6
Jon LaBelle <jon@tech0.com>
2012-10-30 17:19:57 -0500

    Remove calls to begin, end edit object.

--- Trimmer.py
+++ Trimmer.py
@@ -9,12 +9,9 @@ class TrimmerCommand(sublime_plugin.TextCommand):
         trailing_white_space = view.find_all("[\t ]+$")
         trailing_white_space.reverse()
 
-        edit = view.begin_edit()
-
         for r in trailing_white_space:
             view.erase(edit, r)
 
-        view.end_edit(edit)
         sublime.set_timeout(lambda: self.save(view), 10)
 
     def save(self, view):
